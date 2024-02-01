1f7486af88542a95740362830270478f8a4adc36
Emmanuel HOLONOU <manelhln00@gmail.com>
2024-02-01 11:28:09 +0100

    First commit

---
+++ autoplus_api/src/main/java/com/api/autoplus/AutoplusApplication.java
@@ -0,0 +1,13 @@
+package com.api.autoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoplusApplication.class, args);
+	}
+
+}
